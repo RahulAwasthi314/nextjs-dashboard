0e801aa39151f4bcc877d5cfd96103b9820257ce
Rahul Awasthi <awasthir314@gmail.com>
2024-09-27 13:07:06 +0530

    removed user import typo in auth

--- auth.ts
+++ auth.ts
@@ -1,9 +1,10 @@
-import NextAuth, { User } from 'next-auth';
+import NextAuth from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
 import { authConfig } from './auth.config';
 import { sql } from '@vercel/postgres';
 import { z } from 'zod';
 import bcrypt from 'bcrypt';
+import { User } from './app/lib/definitions';
  
 async function getUser(email: string): Promise<User | undefined> {
   try {
